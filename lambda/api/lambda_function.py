ab4c33dd1e81aa5729adce7e0c482daa3ca4232a
ミウラ <39962788+DaichiMiura@users.noreply.github.com>
2023-08-28 14:55:01 +0900

    Update lambda_function.py

--- lambda/api/lambda_function.py
+++ lambda/api/lambda_function.py
@@ -1,6 +1,11 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
-  print(event)
-  print(context)
+  ssm = boto3.client("ssm", region_name="ap-northeast-1")
+  ssm_response = ssm.get_parameters(
+    Names=["/miura/test"]
+  )
+  print(ssm_response)
+  print(ssm_response["Parameters][0]["Value"]
   return json.dumps({"test": "teste"})
